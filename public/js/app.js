25e527358738daf425a3cb5d92ca6aa94ff552f5
Jal Panchal <jal08panchal@gmail.com>
2019-03-26 06:34:05 -0700

    updating fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ console.log('Client side javascript file is loaded!')
 const weatherForm = document.querySelector('form');
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#messageOne");
+const messageTwo = document.querySelector("#messageTwo");
+
 
 weatherForm.addEventListener("submit", (event)=>{
     event.preventDefault(); 
@@ -19,6 +21,7 @@ weatherForm.addEventListener("submit", (event)=>{
             console.log(data.location)
             console.log(data.forecast)
             messageOne.textContent = data.forecast;
+            messageTwo.textContent = data.location;
         }
     })
 })
